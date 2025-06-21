a136012f09264ed3f819a33eba8906120de7d7f5
John Ackermann N8UR <jra@febo.com>
2025-06-21 15:27:49 +0000

    Update example

--- get_gnss_ftp.sh
+++ get_gnss_ftp.sh
@@ -6,23 +6,5 @@
     --organization "HamSci TEC Project" \
     --user "John Ackermann N8UR" \
     --antenna_type "TRM41249.00" \
-    --station_llh "39.7285210 -84.1782042 247.686" \
-    --sftp_host files.tapr.org --sftp_user n8ur-sftp --sftp_pass 9192MHz
-
-/usr/local/bin/get_gnss_ftp.py \
-    -m ./ -f netrs-1-admin.febo.com \
-    --station hs00-n8ur-netrs-1 \
-    --organization "HamSci TEC Project" \
-    --user "John Ackermann N8UR" \
-    --antenna_type "TRM41249.00" \
-    --station_llh "39.7285210 -84.1782042 247.686" \
-    --sftp_host files.tapr.org --sftp_user n8ur-sftp --sftp_pass 9192MHz
-
-/usr/local/bin/get_gnss_ftp.py \
-    -m ./ -f mosaic-t1-admin.febo.com \
-    --station hs00-n8ur-mosaic-t1 \
-    --organization "HamSci TEC Project" \
-    --user "John Ackermann N8UR" \
-    --antenna_type "TRM41249.00" \
-    --station_llh "39.7285210 -84.1782042 247.686" \
-    --sftp_host files.tapr.org --sftp_user n8ur-sftp --sftp_pass 9192MHz
+    --station_llh "39.72 -84.17 247.1" \
+    --sftp_host files.tapr.org --sftp_user xxxx --sftp_pass xxxx
